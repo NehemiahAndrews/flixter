79a3c1deacc351cf22ac7095c08b2ba08526ed31
Nehemiah Andrews <nehemiah.andrews@gmail.com>
2015-07-31 02:26:08 +0000

    Added student show page and linked to

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,4 +2,12 @@ class CoursesController < ApplicationController
   def index
     @courses = Course.all
   end
+
+  def show
+    if Course.where(:id => params[:id]).blank?
+      render :text => "Not Found",  :status => :not_found  
+    else
+      @course = Course.find(params[:id])
+    end
+  end
 end
