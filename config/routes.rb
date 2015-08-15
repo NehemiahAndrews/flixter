c4a2107bb5d7aad51bffce59598ea1595e3194e3
Nehemiah Andrews <nehemiah.andrews@gmail.com>
2015-08-15 20:50:03 +0000

    Styled course show page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Flixter::Application.routes.draw do
 
   resources :courses, :only => [:index, :show]
   resources :lessons, :only => [:show]
+  
   namespace :instructor do
     resources :sections, :only => [] do
       resources :lessons, :only => [:new, :create]
