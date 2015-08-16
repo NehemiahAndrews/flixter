36014e786b00a0d1c7353fbe38359dacba2b2e9b
Nehemiah Andrews <nehemiah.andrews@gmail.com>
2015-08-16 16:10:09 +0000

    Added enrollment gate for lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,6 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user!
+  before_action :require_user_enrollment, :only => [:show]
 
   def show
     if !lesson_exists
@@ -11,6 +12,14 @@ class LessonsController < ApplicationController
 
   helper_method :current_lesson
 
+  def require_user_enrollment
+    if lesson_exists
+      if !current_user.enrolled_in?(current_lesson.section.course)
+        redirect_to course_path(current_lesson.section.course), :alert => 'You must be enrolled to view this lesson'
+      end
+    end
+  end
+
   def lesson_exists
     !Lesson.where(:id => params[:id]).blank?
   end
@@ -18,6 +27,5 @@ class LessonsController < ApplicationController
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
-
 end
 
--- test/controllers/lessons_controller_test.rb
+++ test/controllers/lessons_controller_test.rb
@@ -1,15 +1,26 @@
 require 'test_helper'
 
 class LessonsControllerTest < ActionController::TestCase
-   test "show, found, signed in" do
+   test "show, found, signed in, enrolled" do
+    lesson = FactoryGirl.create(:lesson)    
+    enrollment = FactoryGirl.create(:enrollment, course_id: lesson.section.course.id)
+    sign_in enrollment.user
+
+    get :show, :id => lesson.id
+
+    assert_response :success
+  end
+
+  test "show, found, signed in, not enrolled" do
     user = FactoryGirl.create(:user)
     sign_in user
 
     lesson = FactoryGirl.create(:lesson)    
     get :show, :id => lesson.id
 
-    assert_response :success
+    assert_redirected_to course_path(lesson.section.course.id)
   end
+
 
   test "show, found, not signed in" do
     lesson = FactoryGirl.create(:lesson)    
